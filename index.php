51e55f029a3fa0d2b615d86051eff27429eb8421
Andres cueva <andresanell14@gmail.com>
2020-11-05 21:19:31 -0500

    se modifico login

--- index.php
+++ index.php
@@ -13,48 +13,41 @@ include 'models/UserModel.php';
 include 'controllers/UserController.php';
 
 ?>
+
+	
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Login</title>
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/datepicker3.css" rel="stylesheet">
-	<link href="css/styles.css" rel="stylesheet">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>LOGIN</title>
+	<link rel="stylesheet" href="css/r.scss">
 </head>
 <body>
-	<div class="row">
-		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-			<div class="login-panel panel panel-default">
-				<div class="panel-heading">Login</div>
-				<div class="panel-body">
-					<form action="" method="post" >
-						<fieldset>
-							<div class="form-group">
-								<input class="form-control" placeholder=" Usuario" name="usuario" type="text" >
+		<div class="wrapper fadeInDown">
+  				<div id="formContent">
+	  						 <!-- Icon -->
+	  					 <div class="fadeIn first">
 							</div>
-							<div class="form-group">
-								<input class="form-control" placeholder=" Contraseña" name="contraseña" type="password" >
-							</div>
-							<div class="checkbox">
-								<label>
-									<input name="remember" type="checkbox" value="Remember Me">recordarme
-								</label>
-							</div>
-							<div>
-								<button type="submit" name="submit" class="btn btn-primary">Inciar Sesión</button>
-							</div>
-						</fieldset>
-					</form>
-				</div>
-			</div>
-		</div><!-- /.col-->
-	</div><!-- /.row -->	
-	
+					<!-- Tabs Titles -->
+							<h2 class="active"> Sign In </h2>
+							<h2 class="inactive underlineHover">Sign Up </h2>
+							<div><img src="" alt="" srcset=""></div>
+   					 <!-- Login Form -->
+   					 <form method="POST" action=""> 
+      						<input type="text" id="usuario" class="fadeIn second" name="usuario" placeholder="usuario">
+	  						<input type="text" id="contraseña" class="fadeIn third" name="contraseña" placeholder="contraseña">
+	  					<div class="checkbox">
+							<label>
+								<input name="remember" type="checkbox" value="Remember Me">recordarme
+							</label>
+						</div>
+     							 <input type="submit" name="submit" class="fadeIn fourth" value="INICIAR SESION">
 
-<script src="js/jquery-1.11.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+   							 <!-- Remind Passowrd -->
+ 								 </div>
+							</div>
+					</form>
 <?php 
 
 include "partials/footer.php";
